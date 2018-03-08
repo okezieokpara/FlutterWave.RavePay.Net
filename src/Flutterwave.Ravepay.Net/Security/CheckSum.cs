3dce3d1ca625ce6c7841f43c584c6c7154c15d37
okezieokpara <okeziestanley@gmail.com>
2018-03-08 07:47:15 +0100

    implemented checksum

--- src/Flutterwave.Ravepay.Net/Security/CheckSum.cs
+++ src/Flutterwave.Ravepay.Net/Security/CheckSum.cs
@@ -8,7 +8,7 @@ using Flutterwave.Ravepay.Net.Reflection;
 
 namespace Flutterwave.Ravepay.Net.Security
 {
-   public static class CheckSum
+    public static class CheckSum
     {
         public static string CreateCheckSum(object payParams, string secretKey)
         {
@@ -31,14 +31,14 @@ namespace Flutterwave.Ravepay.Net.Security
                 hashString = hashString + SanitizeNumbers(ReflectionUtil.GetObjectPropValues(payParams, propName));
             }
 
-            return hashString + secretKey;
+            return Hashing.GenerateSHA256String(hashString + secretKey);
         }
 
         private static string SanitizeNumbers(string inputStr)
         {
             if (decimal.TryParse(inputStr, out var sample))
             {
-               
+
                 var result = string.Format("{0:G29}", decimal.Parse(inputStr));
                 return result;
             }
---
+++ src/Flutterwave.Ravepay.Net/Security/Hashing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Flutterwave.Ravepay.Net.Security
+{
+    internal class Hashing
+    {
+        internal static string GenerateSHA256String(string inputString)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            byte[] bytes = Encoding.UTF8.GetBytes(inputString);
+            byte[] hash = sha256.ComputeHash(bytes);
+            return GetStringFromHash(hash);
+        }
+
+        internal static string GenerateSHA512String(string inputString)
+        {
+            SHA512 sha512 = SHA512Managed.Create();
+            byte[] bytes = Encoding.UTF8.GetBytes(inputString);
+            byte[] hash = sha512.ComputeHash(bytes);
+            return GetStringFromHash(hash);
+        }
+
+        private static string GetStringFromHash(byte[] hash)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                result.Append(hash[i].ToString("X2"));
+            }
+            return result.ToString();
+        }
+
+    }
+}
