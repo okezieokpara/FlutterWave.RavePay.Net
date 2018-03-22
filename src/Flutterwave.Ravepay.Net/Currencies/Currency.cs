21c98dbed3efeb4872634dc68d4d2e2dbf8c8eac
okezieokpara <okeziestanley@gmail.com>
2018-03-22 21:52:57 +0100

    test coverage on currency

--- src/Flutterwave.Ravepay.Net/Currencies/Currency.cs
+++ src/Flutterwave.Ravepay.Net/Currencies/Currency.cs
@@ -4,7 +4,33 @@
 
     public class Currency
     {
+        /// <summary>
+        /// Instatiates a type of currency with the name and code
+        /// </summary>
+        /// <param name="name">the name of the currency e.g dollar</param>
+        /// <param name="code">the code of the currency e.g "USD"</param>
+        public Currency(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+        /// <summary>
+        /// Instatiates a type fo currency
+        /// </summary>
+        /// <param name="name">The name of the currency</param>
+        /// <param name="currencyType">A value from the <see cref="CurrencyType"/>enum</param>
+        public Currency(string name, CurrencyType currencyType)
+        {
+            Name = name;
+            Code = CurrencyUtil.GetCurrencyString(currencyType);
+        }
+        /// <summary>
+        /// Represents the name of the currency e.g Dollar
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Represents the code of the currency e.g "USD"
+        /// </summary>
         public string Code { get; set; }
     }
     internal static class CurrencyUtil
